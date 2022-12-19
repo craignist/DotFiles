ede2e858e42ccc5dc91cf8e3d8a947e9406cc256
craignist <nelson@nist.gov>
2022-12-19 13:29:59 -0700

    ADD config for tmux and lvim

---
+++ lvim/bootstrap.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -s ~/.dotfiles/lvim/config.lua ~/.config/lvim/config.lua
---
+++ tmux/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -LO http://invisible-island.net/datafiles/current/terminfo.src.gz
+gunzip terminfo.src.gz
+tic -x terminfo.src
+sudo apt install copyq
+sudo apt install tmux
+sudo apt install xclip
+sudo apt install zoxide
+sudo apt install fzf
+python3 -m pip install libtmux --user
+python3 -m pip install dataclasses --user
