ad6258200b9a4606c6e4fb8ccd0044e2c9d29052
craignist <nelson@nist.gov>
2022-12-20 16:09:16 -0700

    ADD tmux configs

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,6 +2,7 @@
 curl -LO http://invisible-island.net/datafiles/current/terminfo.src.gz
 gunzip terminfo.src.gz
 tic -x terminfo.src
+rm ./terminfo.src
 sudo apt install copyq
 sudo apt install tmux
 sudo apt install xclip
@@ -9,3 +10,5 @@ sudo apt install zoxide
 sudo apt install fzf
 python3 -m pip install libtmux --user
 python3 -m pip install dataclasses --user
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sf ~/.dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf
